4111c848b9cb40d259c2f1f9f26eb7e081190398
James Wood <james.s.wood@icloud.com>
2021-01-27 11:34:38 -0500

    initial commit

---
+++ LowestNumber.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace LowestNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Starting variables 
+      // Use built-in methods and save to variable 
+      double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+
+
+      // Use built-in methods and save to variable 
+      double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+
+      // Print the lowest number
+      Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+
+    }
+  }
+}
